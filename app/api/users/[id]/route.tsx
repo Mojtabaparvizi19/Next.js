8b9b0cd499e74c1c2182867f1e635833e3cd9fd1
Mojtaba <mojtabaparvizi19@gmail.com>
2024-09-14 16:49:08 -0400

    update user

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -58,9 +58,32 @@ export async function PUT(request: NextRequest, { params: { id } }: Props) {
 
   const body: BodyProp = await request.json();
   const validation = schema.safeParse(body);
+
   if (!validation.success) {
     return NextResponse.json(validation.error.errors, { status: 400 });
   }
+
+  const userToUpdate = await prisma.user.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
+
+  if (!userToUpdate) {
+    return NextResponse.json({ error: "User does not exist" }, { status: 404 });
+  }
+
+  const updatedUser = await prisma.user.update({
+    where: {
+      id: userToUpdate.id,
+    },
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+
+  return NextResponse.json(updatedUser, { status: 201 });
 }
 
 export function DELETE(request: NextRequest, { params: { id } }: Props) {
