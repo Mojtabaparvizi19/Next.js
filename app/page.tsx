1190d017f40c56a25568042e0db263d54817778b
Mojtaba <mojtabaparvizi19@gmail.com>
2024-09-22 18:01:47 -0400

    lazy loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,32 @@
+"use client";
+
 import UserCard from "./components/UserCard/UserCard";
 import { getServerSession } from "next-auth";
-import { authOption } from "./api/auth/[...nextauth]/route";
+import authOption from "./api/auth/authOption";
 import Image from "next/image";
+import image from "./assets/images/london.jpg";
+import { CSSProperties, useState } from "react";
+import dynamic from "next/dynamic";
 
-export default async function Home() {
-  const session = await getServerSession(authOption);
+const imageCSS: CSSProperties = {
+  height: "400px",
+  width: "400px",
+  background: "red",
+  padding: "10px",
+  position: "relative",
+};
+
+const HeavyComponent = dynamic(() => import("./components/UserCard/UserCard"), {
+  loading: () => (
+    <div>
+      <hr /> <p>...loading</p>
+    </div>
+  ),
+});
+
+export default function Home() {
+  const [isLoaded, setLoaded] = useState(false);
+  // const session = await getServerSession(authOption);
 
   return (
     <main>
@@ -12,7 +34,23 @@ export default async function Home() {
         Hello<span className="font-bold"> {session?.user?.name}</span>
       </h1>
       <UserCard /> */}
-      <Image />
+
+      <div style={imageCSS}>
+        <Image
+          style={{ borderRadius: "10px" }}
+          // width={100}
+          // height={300}
+          sizes="inherit"
+          fill
+          objectFit="cover"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/London_Big_Ben_Phone_box.jpg/800px-London_Big_Ben_Phone_box.jpg"
+          alt="image of London"
+        />
+      </div>
+      <button onClick={() => setLoaded(true)} className="btn">
+        show
+      </button>
+      {isLoaded && <HeavyComponent />}
     </main>
   );
 }
